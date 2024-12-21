5c91a6f66ef8e9b66efc9a054d9809da85e411e3
Artem Bilko <Art.Bilko14@gmail.com>
2024-12-21 10:53:40 +0200

    animate website

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import './menu.js';
 import './scrollFix.js';
 import './master-language.js';
 import './students-slider.js';
+
+AOS.init({
+  once: true,
+  disable: 'mobile'
+});
--- src/students-slider.js
+++ src/students-slider.js
@@ -2,23 +2,20 @@ const DESKTOP_BREAKPOINT = "1280";
 
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
-  slidesPerView: 3,
   autoplay: true,
+  slidesPerView: 1,
+  spaceBetween: 32,
+  loop: true,
 
   breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 32,
-      loop: true,
-    },
     768: {
       slidesPerView: 2,
       spaceBetween: 32,
-      loop: true,
     },
     1280: {
       slidesPerView: 3,
       spaceBetween: 32,
+      loop: false,
     }
   },
   on: {
